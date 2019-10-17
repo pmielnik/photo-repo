c8764c9d226324b1b2aa39ca7fe3d7be3fde59c8
pmielnik <patricia.mielnik1@gmail.com>
2019-10-17 00:39:14 -0400

    non-unique tags + cleanup

--- app/prepo/photorepo/models.py
+++ app/prepo/photorepo/models.py
@@ -4,11 +4,10 @@ from django.urls import reverse
 
 # Create your models here.
 class Photo(models.Model):
-    _id         = models.CharField(unique=True, max_length=500, primary_key=True)
+    _id         = models.BigAutoField(primary_key=True)
     photoName   = models.CharField(max_length=500)
     # authorId    = models.ForeignKey('User', on_delete=models.CASCADE)
     # author      = models.CharField(max_length=500)
-    tags        = models.CharField(max_length=1000, unique=True)
+    tags        = models.CharField(max_length=1000)     #Will be stored as a string rep of JSON object
     date        = models.DateField(auto_now=True)
-    image       = models.ImageField(storage=PhotoStorage()) # TODO: firgure out how to add proper url field???
-    # image       = models.ImageField(upload_to='images/') 
+    image       = models.ImageField(storage=PhotoStorage())
